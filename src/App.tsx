73cd39750aeadb068a0c1371a7a7c88930c8e9b0
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-28 00:16:10 +0100

    Remove div

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import type { HTMLAttributes } from 'react'
 import styled from 'styled-components'
 import { PageContainer } from 'lugo'
 import { breakpoint, spacing } from './style/theme'
 import Board from './components/Board'
 
-const Container = styled(PageContainer)`
+const App = styled(({ ...props }) => (
+  <PageContainer
+    {...props}
+    maxWidth={breakpoint.laptop}
+    sideMargin={spacing.viewportHeight}
+  >
+    <Board />
+  </PageContainer>
+))`
   padding: 0 ${spacing.viewportHeight};
 
-  @media screen and (${spacing.tabletMaxWidth}) {
-    padding: 0 calc(${spacing.viewportHeight} / 2);
-  }
-`
-
-const App = styled((props: HTMLAttributes<HTMLDivElement>) => (
-  <div {...props}>
-    <Container maxWidth={breakpoint.laptop} sideMargin={spacing.viewportHeight}>
-      <Board />
-    </Container>
-  </div>
-))`
   margin-top: ${spacing.viewportHeight};
   margin-bottom: ${spacing.viewportHeight};
 
   @media screen and (${spacing.tabletMaxWidth}) {
+    padding: 0 calc(${spacing.viewportHeight} / 2);
+
     margin-top: calc(${spacing.viewportHeight} / 2);
     margin-bottom: calc(${spacing.viewportHeight} / 2);
   }
