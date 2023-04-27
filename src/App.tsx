c2430583e267a76ac8ea9a9a6996be16ab7e6ccb
Ludovico Forace <ludovicoforace@yahoo.com>
2023-04-28 00:24:40 +0100

    Remove unnecessary div (#4)
    * Remove div
    
    * Specify type
    
    * Do not pass prop

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,19 @@ import { PageContainer } from 'lugo'
 import { breakpoint, spacing } from './style/theme'
 import Board from './components/Board'
 
-const Container = styled(PageContainer)`
+const App = styled((props: HTMLAttributes<HTMLDivElement>) => (
+  <PageContainer {...props} maxWidth={breakpoint.laptop}>
+    <Board />
+  </PageContainer>
+))`
   padding: 0 ${spacing.viewportHeight};
 
-  @media screen and (${spacing.tabletMaxWidth}) {
-    padding: 0 calc(${spacing.viewportHeight} / 2);
-  }
-`
-
-const App = styled((props: HTMLAttributes<HTMLDivElement>) => (
-  <div {...props}>
-    <Container maxWidth={breakpoint.laptop} sideMargin={spacing.viewportHeight}>
-      <Board />
-    </Container>
-  </div>
-))`
   margin-top: ${spacing.viewportHeight};
   margin-bottom: ${spacing.viewportHeight};
 
   @media screen and (${spacing.tabletMaxWidth}) {
+    padding: 0 calc(${spacing.viewportHeight} / 2);
+
     margin-top: calc(${spacing.viewportHeight} / 2);
     margin-bottom: calc(${spacing.viewportHeight} / 2);
   }
